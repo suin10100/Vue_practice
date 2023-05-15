3661c133f3ad528f3c5f569e221c5acaa54cb152
suin10100 <94846590+suin10100@users.noreply.github.com>
2023-05-15 17:26:38 +0900

    Events and Events Modifiers
    Tutotial 5~7

--- Turotial-5~6/app.js
+++ Turotial-5~6/app.js
@@ -15,6 +15,10 @@ new Vue ({
         updateXY:function(event){
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        click:function(){
+            alert('You cliciked me');
         }
+
     }
 })
