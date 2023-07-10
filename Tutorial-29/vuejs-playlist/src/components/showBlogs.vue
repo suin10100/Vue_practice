b390193a92ba6596a46b2fd2ebbda8862f94f6e1
suin10100 <94846590+suin10100@users.noreply.github.com>
2023-07-10 16:32:23 +0900

    Update showBlogs.vue
    checking errors and fixed it
    it was just https -> http.. :(

--- Tutorial-29/vuejs-playlist/src/components/showBlogs.vue
+++ Tutorial-29/vuejs-playlist/src/components/showBlogs.vue
@@ -1,44 +1,40 @@
 <template>
     <div id="show-blogs">
-      <h1> All blog Articles</h1>
-      <div v-for ="blog in blogs" class ="single-blog"></div>
-      <h2> {{ blog.title | to-uppercase }}</h2>
-      <article>{{ blog.body }}</article>
+        <h1>All Blog Articles</h1>
+        <div v-for="blog in blogs" class="single-blog">
+            <h2>{{ blog.title }}</h2>
+            <article>{{ blog.body }}</article>
+        </div>
     </div>
-  </template>
-  
-  <script>
+</template>
 
-  export default {
-    
+<script>
+export default {
     data () {
-      return {
-        blogs: []
-      }
+        return {
+            blogs: []
+        }
+    },
+    methods: {
+
     },
     created() {
-        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-            this.blogs = data.body.slice(0, 10);
-             
+        this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.blogs = data.body.slice(0,10);
+        });
+    }
+}
+</script>
 
-        })
-    }
-  }
-  </script>
-  
-  <style>
-  body {
-    #show-blogs{
-        max-width: 800px;
-        margin: 0 auto;
-    }
-  }
-  .single-blog{
-        padding: 20px;
-        margin: 20px 0;
-        box-sizing: border-box;
-        background: #eee;
-  }
-  
-  </style>
-  
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
