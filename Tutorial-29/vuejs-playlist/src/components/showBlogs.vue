ef278a89718ecaf9b06e0df5f13dea44a267783e
suin10100 <94846590+suin10100@users.noreply.github.com>
2023-07-10 17:03:39 +0900

    Tutorial 35
    making custom directives

--- Tutorial-29/vuejs-playlist/src/components/showBlogs.vue
+++ Tutorial-29/vuejs-playlist/src/components/showBlogs.vue
@@ -1,8 +1,8 @@
 <template>
-    <div id="show-blogs">
+    <div v-theme:column="'wide'" id="show-blogs">
         <h1>All Blog Articles</h1>
         <div v-for="blog in blogs" class="single-blog">
-            <h2>{{ blog.title }}</h2>
+            <h2 v-rainbow>{{ blog.title }}</h2>
             <article>{{ blog.body }}</article>
         </div>
     </div>
@@ -24,6 +24,10 @@ export default {
         });
     }
 }
+// http -> 브라우저와 서버간 데이터를 주고 받는 통신 프로토콜
+// http request = 데이터 request
+// get -> 서버에서 데이터를 가져옴 서버 주소인 /api/data 로부터 값을 불러 올 때 사용
+// axios 요청 시 파라메터 정보(/api/todos/1)를 같이 입력하여 정보를 얻어 올 수 있다.
 </script>
 
 <style>
