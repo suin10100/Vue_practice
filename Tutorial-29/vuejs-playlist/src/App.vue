1625162396dc8f39eae524091d42df788ffc08bb
suin10100 <94846590+suin10100@users.noreply.github.com>
2023-07-11 16:59:27 +0900

    ~Tutorial 39
    install webpack cli and making a router .
    But it has a error so i will fix it.

--- Tutorial-29/vuejs-playlist/src/App.vue
+++ Tutorial-29/vuejs-playlist/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <!-- <add-blog></add-blog> -->
-    <show-blogs></show-blogs>
+    <!-- <show-blogs></show-blogs>
+    <list-Blogs></list-Blogs> -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import addBlog from './components/addBlog.vue'
+import addBlog from './components/addBlog.vue'
 import showBlogs from './components/showBlogs.vue'
+import listBlogs from './components/listBlogs.vue'
+
 export default {
   components: {
-    // 'add-blog': addBlog,
-    'show-blogs': showBlogs
+    'add-blog': addBlog,
+    'show-blogs': showBlogs,
+    'list-Blogs': listBlogs
   },
   data () {
     return {
--- Tutorial-29/vuejs-playlist/src/components/listBlogs.vue
+++ Tutorial-29/vuejs-playlist/src/components/listBlogs.vue
@@ -1,17 +1,16 @@
 <template>
     <div id="show-blogs">
-        <h1>All Blog Articles</h1>
+        <h1>List Blog Titles</h1>
         <input type="text" v-model="search" placeholder="search blogs" class="searchBar" />
         <div v-for="blog in filteredBlogs" class="single-blog">
             <h2 v-rainbow>{{ blog.title | toUppercase }}</h2>
-            <article>{{ blog.body }}</article>
+
         </div>
     </div>
 </template>
 
 <script>
 import searchMixin from '../mixins/searchMixin';
-
 export default {
     data () {
         return {
@@ -20,20 +19,10 @@ export default {
             // 검색할 값 
         }
     },
-    methods: {
-
-    },
     created() {
         this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
             this.blogs = data.body.slice(0, 10);
         });
-    },
-    computed: {
-        // filteredBlogs: function(){
-        //     return this.blogs.filter((blog) => {
-        //         return blog.title.match(this.search);
-        //     });
-        // }
     },
     filters: {
         toUppercase(value){
@@ -48,7 +37,9 @@ export default {
             }
         }
     },
-    mixins: [searchMixin]
+    mixins: [
+        searchMixin
+    ]
 } 
 // http -> 브라우저와 서버간 데이터를 주고 받는 통신 프로토콜
 // http request = 데이터 request
