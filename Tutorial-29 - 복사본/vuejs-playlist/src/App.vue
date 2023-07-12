0af0f44683b7b611bce340942a4064cd6a9aa955
suin10100 <94846590+suin10100@users.noreply.github.com>
2023-07-12 21:51:59 +0900

    Tutorial-40
    making header

---
+++ Tutorial-29 - 복사본/vuejs-playlist/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <add-blog></add-blog> -->
+    <!-- <show-blogs></show-blogs>
+    <list-Blogs></list-Blogs> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+import listBlogs from './components/listBlogs.vue'
+
+export default {
+  components: {
+    'add-blog': addBlog,
+    'show-blogs': showBlogs,
+    'list-Blogs': listBlogs
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+
+</style>
